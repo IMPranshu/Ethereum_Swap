2a5c0d772e0c959aca3c453fff73ae686429cf5c
P.K. Agrawal <pranshukumar1999@gmail.com>
2021-12-18 12:51:00 +0530

    Test for exchange writtten and gloabal declaration added

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -4,19 +4,29 @@ const EthSwap = artifacts.require('Ethswap')
 
 require('chai').use(require('chai-as-promised')).should()
 contract('Ethswap',(accounts) =>{
+let token, ethSwap
+	before(async() => {
+		token = await Token.new()
+		ethSwap = await EthSwap.new()
+		await token.transfer(ethSwap.address,'1000000000000000000000000')
+
+	})
 
 	describe('Token Deployement', async() => {
 		it('contract has a name',async() =>{
-			let token = await Token.new()
 			const name = await token.name()
 			assert.equal(name,'DApp Token')
 		})
 	})
 	describe('EthSwap Deployement', async() => {
 		it('contract has a name',async() =>{
-			let ethSwap = await EthSwap.new()
 			const name = await ethSwap.name()
 			assert.equal(name,'EthSwap Instant Exchange')
 		})
+		it('contract has tokens', async() =>{
+			let balance = await token.balanceOf(ethSwap.address)
+			assert.equal(balance.toString(),'1000000000000000000000000')
+
+		}) 
 	})
 })
