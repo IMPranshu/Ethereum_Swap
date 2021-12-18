b5001120bf709466d916ac846cbfd8b6891303f9
P.K. Agrawal <pranshukumar1999@gmail.com>
2021-12-18 12:41:23 +0530

    Text script for Token added

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -5,6 +5,13 @@ const EthSwap = artifacts.require('Ethswap')
 require('chai').use(require('chai-as-promised')).should()
 contract('Ethswap',(accounts) =>{
 
+	describe('Token Deployement', async() => {
+		it('contract has a name',async() =>{
+			let token = await Token.new()
+			const name = await token.name()
+			assert.equal(name,'DApp Token')
+		})
+	})
 	describe('EthSwap Deployement', async() => {
 		it('contract has a name',async() =>{
 			let ethSwap = await EthSwap.new()
